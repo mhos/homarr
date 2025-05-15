60bbe93f0ec4f789ca3016e9c3c28ea10d0723f9
mhos <junk@vbelks.org>
2025-05-14 21:19:56 -0400

    fix: Update UPS monitor widget to use proper factory pattern and exports

--- packages/widgets/src/ups-monitor/index.ts
+++ packages/widgets/src/ups-monitor/index.ts
@@ -3,22 +3,24 @@ import { IconBolt } from "@tabler/icons-react";
 import { createWidgetDefinition } from "../definition";
 import { optionsBuilder } from "../options";
 
-export const { definition, withDynamicImport } = createWidgetDefinition("upsMonitor", {
+export const { definition, componentLoader } = createWidgetDefinition("upsMonitor", {
   icon: IconBolt,
   supportedIntegrations: ["upsMonitor"],
   integrationsRequired: true,
-  createOptions: (settings) => ({
-    refreshInterval: optionsBuilder.number({
-      defaultValue: settings.defaultOptions?.refreshInterval ?? 30,
-      label: "widget.upsMonitor.option.refreshInterval.label",
-      description: "widget.upsMonitor.option.refreshInterval.description",
-      min: 10,
-      step: 5,
-    }),
-    showAdvanced: optionsBuilder.switch({
-      defaultValue: settings.defaultOptions?.showAdvanced ?? false,
-      label: "widget.upsMonitor.option.showAdvanced.label",
-      description: "widget.upsMonitor.option.showAdvanced.description",
-    }),
-  }),
-});
+  createOptions() {
+    return optionsBuilder.from((factory) => ({
+      refreshInterval: factory.number({
+        defaultValue: 30,
+        label: "widget.upsMonitor.option.refreshInterval.label",
+        withDescription: true,
+        min: 10,
+        step: 5,
+      }),
+      showAdvanced: factory.switch({
+        defaultValue: false,
+        label: "widget.upsMonitor.option.showAdvanced.label",
+        withDescription: true,
+      }),
+    }));
+  },
+}).withDynamicImport(() => import("./component"));
